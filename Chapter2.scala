720430b6cb877bbd27c61db6a9169833c0f4b4f9
writetoanuj <writetoanuj@users.noreply.github.com>
2015-07-27 16:57:55 -0400

    Update Chapter2.scala

--- Chapter2.scala
+++ Chapter2.scala
@@ -1,10 +1,26 @@
 
 
 
-# Q9
+// Q9
 
 def recProd(s:String,index:Int=0) : Int = {
     s(index).toInt * { if (index+1 < s.length ) recProd(s,index+1) else 1}
 }
 
 print(recProd("Hello"))
+
+//Q 10
+def powCalc(x:Int, n:Int) : Double = {
+  if (n> 0 && n%2 == 0) {
+    powCalc(x,n/2) * powCalc(x,n/2)
+  }
+  else if (n >0 && n%2 == 1) {
+    x * powCalc(x,n-1)
+  }
+  else if (n==0) {
+    1
+  }
+  else {
+    1/powCalc(x,-1*n)
+  }
+}
