7c1ddd89f5580abda819803b9a3abce90ca030a1
writetoanuj <writetoanuj@users.noreply.github.com>
2015-08-10 16:39:42 -0400

    Update chapter3.scala

--- chapter3.scala
+++ chapter3.scala
@@ -1,3 +1,4 @@
+// Q1
 import util.Random
 
 val a = new Array[Int](10)
@@ -9,4 +10,37 @@ for (i <- 0 until 10) {
 for (i <- 0 until 10) {
     println(a(i))
 }
+
+
+// Q2
+val a = Array(1,2,3,4,5)
+
+for (i <- 0 until a.size) {
+    if ( i % 2 == 0 && i < a.size-1) {
+        val temp = a(i)
+        a(i)=a(i+1)
+        a(i+1)=temp
+    }
+}
+
+for (i<-0 until a.size) {
+    println(a(i))
+}
+
+//Q3
+val a = Array(1,2,3,4,5)
+
+val b = for (i <- 0 until a.size)  yield {
+    if ( i % 2 == 0 && i < a.size-2) {
+        a(i+1)
+    }
+    else if ( i%2 != 0 && i>0) { a(i-1)}
+    else a(i)
+}
+
+for (i<-0 until b.size) {
+    println(b(i))
+}
+
+//Q4 
 
